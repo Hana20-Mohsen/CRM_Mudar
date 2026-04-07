c6068c63e839231bf60a7f3fd2df51602b23b5bb
Hana20-Mohsen <hana2021mohsen@gmail.com>
2026-04-07 11:39:43 +0200

    .

--- frontend/src/socket/socket.js
+++ frontend/src/socket/socket.js
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client"
-console.log(`${import.meta.env.VITE_API_URL}`);
+console.log(`back url : ${import.meta.env.VITE_API_URL}`);
 
 //  transports:["websocket"],
 const socket = io(`${import.meta.env.VITE_API_URL}`,{
