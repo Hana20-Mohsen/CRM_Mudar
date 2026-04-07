2f5cc267d9814347fc1ada500529a6830a88a916
Hana20-Mohsen <hana2021mohsen@gmail.com>
2026-04-07 12:30:22 +0200

    try to fix issue in useCheckIn

--- frontend/src/socket/events/useCheckIn.event.js
+++ frontend/src/socket/events/useCheckIn.event.js
@@ -2,21 +2,23 @@ import { useEffect } from "react";
 import socket from "../socket.js";
 import { useQueryClient } from "@tanstack/react-query";
 
-const useCheckIn =()=>{
+const useCheckIn = () => {
     const queryClient = useQueryClient();
-    useEffect(()=>{
-        socket.on("checkedIn" , (userId)=>{
+    useEffect(() => {
+        socket.on("checkedIn", (userId) => {
             console.log("Socket received:", userId);
-             queryClient.invalidateQueries(['getAttendance'])
+            queryClient.invalidateQueries({
+                queryKey: ['getAttendance'],
+            });
             // queryClient.setQueryData(['getProducts'] , (oldData)=>{
             //     if(!oldData) return newProduct;
             //     return [newProduct, ...oldData];
             // });
         });
-        return ()=>{
+        return () => {
             socket.off("checkedIn");
         }
-    } , [])
+    }, [])
 }
 
 export default useCheckIn;
