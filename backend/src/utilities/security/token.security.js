d2f3037e2e57481ef79b01d5020e337e09c9007e
Hana20-Mohsen <hana2021mohsen@gmail.com>
2026-04-07 14:27:34 +0200

    log to ensure signature

--- backend/src/utilities/security/token.security.js
+++ backend/src/utilities/security/token.security.js
@@ -9,6 +9,8 @@ export const generateToken=({payload={} , signature=process.env.TOKEN_SIGNATURE 
 }
 
 export const verifyToken=({token='' , signature=process.env.TOKEN_SIGNATURE})=>{
+    console.log(`we use signature: `, signature);
+    
     const decoded= jwt.verify(token , signature)
     return decoded
 
