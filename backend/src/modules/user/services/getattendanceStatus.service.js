91df08fa4be3c8dd8142d32638422d982cac7af4
Hana20-Mohsen <hana2021mohsen@gmail.com>
2026-04-19 16:15:38 +0200

    some changes in get attendance status to get only the records that happen today

--- backend/src/modules/user/services/getattendanceStatus.service.js
+++ backend/src/modules/user/services/getattendanceStatus.service.js
@@ -1,14 +1,28 @@
+
 import Attendance from "../../../DB/models/attendance.model.js";
 
 export const getMyAttendanceStatus = async (req, res) => {
   const userId = req.user._id;
 
+  const startOfDay = new Date();
+  startOfDay.setHours(0, 0, 0, 0);
+
+  // نهاية اليوم (23:59:59)
+  const endOfDay = new Date();
+  endOfDay.setHours(23, 59, 59, 999);
+
+  console.log(`startOfDay : ${startOfDay} , endOfDay : ${endOfDay}`);
+  
   const activeAttendance = await Attendance.findOne({
     user: userId,
     checkOutAt: null,
+    date: {
+      $gte: startOfDay,
+      $lte: endOfDay,
+    },
   });
 
-
+  console.log('activeAttendance :' , activeAttendance);
 
   return res.status(200).json({
     isCheckedIn: !!activeAttendance,
