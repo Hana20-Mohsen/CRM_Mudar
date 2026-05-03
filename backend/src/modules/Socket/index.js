5e49feb33d1ee9d149954aa8558648b961de4f94
Hana20-Mohsen <hana2021mohsen@gmail.com>
2026-05-03 15:45:11 +0300

    .

--- backend/src/modules/Socket/index.js
+++ backend/src/modules/Socket/index.js
@@ -34,7 +34,8 @@ const initSocket = (server) => {
 const token = socket.handshake.auth.token
     console.log(`token from socket : `, token);
       const decoded = verifyToken({ token, signature: process.env.TOKEN_SIGNATURE_ADMIN || process.env.TOKEN_SIGNATURE })
-
+      console.log(`decode : ${decoded}`);
+      
       socket.user = decoded
       //   console.log(v);
 
