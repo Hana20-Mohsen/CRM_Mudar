990c107272c8a1237650ac7882fc5f3bd070bf04
Hana20-Mohsen <hana2021mohsen@gmail.com>
2026-07-23 12:56:56 +0300

    update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,9 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
  plugins: [react()],
+ base: "/crm/", 
   server: {
     host: true,
-    base: "/crm/", 
     port: 5173, 
     strictPort: false,
     allowedHosts: [
